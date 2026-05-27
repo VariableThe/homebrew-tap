778a681ba62ab45e381c11f40152c0458a19ed7a
Aditya <slayermk57@gmail.com>
2026-05-27 14:47:59 +0530

    updating to new release

--- Casks/vinyl.rb
+++ Casks/vinyl.rb
@@ -2,7 +2,7 @@ cask "vinyl" do
   version "1.0.2"
   sha256 "9b9eef231a98ed3dca092610aa1b13003eaacf95507f2f9774e978f58abaeb06"
 
-  url "https://github.com/VariableThe/Vinyl/releases/download/Vinyl/Vinyl.zip"
+  url "https://github.com/VariableThe/Vinyl/releases/download/preferences/Vinyl.zip"
   name "Vinyl"
   desc "Native, lightweight macOS menu bar app for synchronized music lyrics"
   homepage "https://github.com/VariableThe/Vinyl"
