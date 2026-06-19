cfb8fea1fae07a0d821f82e980f75d4f265986c9
Aditya <slayermk57@gmail.com>
2026-06-20 00:29:25 +0530

    Update papercache to 0.1.8

--- Casks/papercache.rb
+++ Casks/papercache.rb
@@ -1,8 +1,8 @@
 cask "papercache" do
   arch arm: "arm64"
 
-  version "0.1.7"
-  sha256 arm:   "6270f5aa4a348fda29ee6a7563d17281554e4ceec0c25521605e2324e90c7b68"
+  version "0.1.8"
+  sha256 arm:   "8ff5127931969d0db4c73c37f763a568bffd9c0e90e5fbd8c750e76737d0ca1e"
 
   url "https://github.com/VariableThe/PaperCache/releases/download/v#{version}/PaperCache-#{version}-#{arch}-mac.zip"
   name "PaperCache"
